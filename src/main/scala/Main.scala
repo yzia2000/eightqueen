30d4e6a1ddb1a6d724b5023a8356f1934880bf46
Mohammad Yousuf Minhaj Zia <yzia2000@gmail.com>
2022-12-07 19:30:41 +0800

    feat: refactored recursion to zip the board
    Instead of using sequence (0 until x), zipped the board

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -27,8 +27,9 @@ object Board {
     Vector.fill(n)(Row.default(n))
   }
 
-  def playQueen(board: Board, row: Int, col: Int) = {
-    board.updated(row, board(row).updated(col, Queen))
+  def playQueen(board: Board, coord: (Int, Int)) = coord match {
+    case (row, col) =>
+      board.updated(row, board(row).updated(col, Queen))
   }
 
   def toString(board: Board) = {
@@ -43,12 +44,20 @@ object Board {
   def solve(n: Int, board: Board, numQueens: Int): Option[Board] = {
     if (numQueens == n) Some(board)
     else {
+      val cells =
+        board.zipWithIndex
+          .map({ case (cells, row) =>
+            cells.zipWithIndex.map({ case (cellValue, col) =>
+              (cellValue, (row, col))
+            })
+          })
+          .flatten
+
       val result = for {
-        row <- 0 until n
-        col <- 0 until n
-        updatedBoard <- board(row)(col) match {
-          case Empty if canPlace(n, board, row, col) => {
-            solve(n, playQueen(board, row, col), numQueens + 1)
+        cell <- cells
+        updatedBoard <- cell match {
+          case (Empty, coord) if canPlace(n, board, coord) => {
+            solve(n, playQueen(board, coord), numQueens + 1)
           }
           case _ => None
         }
@@ -57,43 +66,43 @@ object Board {
     }
   }
 
-  def canPlace(n: Int, board: Board, row: Int, col: Int): Boolean = {
-    // Linear Checks
+  def canPlace(n: Int, board: Board, coord: (Int, Int)): Boolean = coord match {
+    case (row, col) =>
+      // Linear Checks
 
-    val checkTopToBottom =
-      (0 until n).foldLeft(0)((agg, x) => agg + board(x)(col).value) > 0
+      lazy val checkTopToBottom =
+        (0 until n).foldLeft(0)((agg, x) => agg + board(x)(col).value) > 0
 
-    val checkLeftToRight =
-      (0 until n).foldLeft(0)((agg, x) => agg + board(row)(x).value) > 0
+      lazy val checkLeftToRight =
+        (0 until n).foldLeft(0)((agg, x) => agg + board(row)(x).value) > 0
 
-    val checkDiagTopLeftBottomRight =
-      (
-        ((row - Seq(row, col).min) until n)
-          .zip(((col - Seq(row, col).min) until n))
-        )
-        .foldLeft(0)((agg, x) =>
-          x match { case (x, y) => board(x)(y).value + agg }
-        ) > 0
+      lazy val checkDiagTopLeftBottomRight =
+        (
+          ((row - Seq(row, col).min) until n)
+            .zip(((col - Seq(row, col).min) until n))
+          )
+          .foldLeft(0)((agg, x) =>
+            x match { case (x, y) => board(x)(y).value + agg }
+          ) > 0
 
-    val checkDiagBottomLeftTopRight =
-      (
-        ((0 until n)
-          .map(x => (row - x, col + x))
-          ++
-            (0 until n)
-              .map(x => (row + x, col - x)))
-          .filter({ case (x, y) => x >= 0 && x < n && y >= 0 && y < n })
-        )
-        .foldLeft(0)((agg, x) =>
-          x match { case (x, y) => board(x)(y).value + agg }
-        ) > 0
-
-    !(checkTopToBottom || checkLeftToRight || checkDiagTopLeftBottomRight || checkDiagBottomLeftTopRight)
+      lazy val checkDiagBottomLeftTopRight =
+        (
+          ((0 until n)
+            .map(x => (row - x, col + x))
+            ++
+              (0 until n)
+                .map(x => (row + x, col - x)))
+            .filter({ case (x, y) => x >= 0 && x < n && y >= 0 && y < n })
+          )
+          .foldLeft(0)((agg, x) =>
+            x match { case (x, y) => board(x)(y).value + agg }
+          ) > 0
+      !(checkTopToBottom || checkLeftToRight || checkDiagTopLeftBottomRight || checkDiagBottomLeftTopRight)
   }
 }
 
 @main def main: Unit = {
-  val n = 5
+  val n = 8
   val board = Board.default(n)
   print(
     Board
