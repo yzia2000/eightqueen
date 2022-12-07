86288aea6a85c7a3b08124370a7036ee062a01d9
Mohammad Yousuf Minhaj Zia <yzia2000@gmail.com>
2022-12-07 19:30:11 +0800

    feat: implemented working version of n queen

---
+++ build.sbt
@@ -0,0 +1,15 @@
+val scala3Version = "3.2.1"
+
+lazy val root = project
+  .in(file("."))
+  .settings(
+    name := "eightqueen",
+    version := "0.1.0-SNAPSHOT",
+    scalaVersion := scala3Version,
+    libraryDependencies ++= Seq(
+      "eu.timepit" %% "refined" % "0.10.1",
+      "dev.zio" %% "zio" % "2.0.5",
+      "dev.zio" %% "zio-streams" % "2.0.5",
+      "org.scalameta" %% "munit" % "0.7.29" % Test
+    )
+  )
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,98 @@
+import zio._
+import zio.stream._
+
+sealed trait CellValue {
+  val value: Int
+}
+
+case object Queen extends CellValue {
+  val value = 1
+}
+case object Empty extends CellValue {
+  val value = 0
+}
+
+case class Cell(value: CellValue)
+type Row = Vector[CellValue]
+type Board = Vector[Row]
+
+object Row {
+  def default(n: Int): Row = {
+    Vector.fill(n)(Empty)
+  }
+}
+
+object Board {
+  def default(n: Int): Board = {
+    Vector.fill(n)(Row.default(n))
+  }
+
+  def playQueen(board: Board, row: Int, col: Int) = {
+    board.updated(row, board(row).updated(col, Queen))
+  }
+
+  def toString(board: Board) = {
+    board.map(_.mkString(", ")).mkString("\n")
+  }
+
+  def tap[T](x: T) = {
+    println(x)
+    x
+  }
+
+  def solve(n: Int, board: Board, numQueens: Int): Option[Board] = {
+    if (numQueens == n) Some(board)
+    else {
+      val result = for {
+        row <- 0 until n
+        col <- 0 until n
+        updatedBoard <- board(row)(col) match {
+          case Empty if canPlace(n, board, row, col) => {
+            solve(n, playQueen(board, row, col), numQueens + 1)
+          }
+          case _ => None
+        }
+      } yield (updatedBoard)
+      result.headOption
+    }
+  }
+
+  def canPlace(n: Int, board: Board, row: Int, col: Int): Boolean = {
+    // Linear Checks
+
+    lazy val checkTopToBottom =
+      (0 until n).foldLeft(0)((agg, x) => agg + board(x)(col).value) > 0
+
+    lazy val checkLeftToRight =
+      (0 until n).foldLeft(0)((agg, x) => agg + board(row)(x).value) > 0
+
+    // Diagonal Checks
+
+    lazy val checkDiagTopLeftBottomRight =
+      ((row - Seq(row, col).min) until n)
+        .zip(((col - Seq(row, col).min) until n))
+        .foldLeft(0)((agg, x) =>
+          x match { case (x, y) => board(x)(y).value + agg }
+        ) > 0
+
+    lazy val checkDiagBottomLeftTopRight =
+      ((row - Seq(row, col).min) until n)
+        .zip(((col - Seq(row, col).min) until n))
+        .map({ case (x, y) => (x, n - 1 - y) })
+        .foldLeft(0)((agg, x) =>
+          x match { case (x, y) => board(x)(y).value + agg }
+        ) > 0
+
+    !(checkTopToBottom || checkLeftToRight || checkDiagTopLeftBottomRight || checkDiagBottomLeftTopRight)
+  }
+}
+
+@main def main: Unit = {
+  val n = 7
+  val board = Board.default(n)
+  print(
+    Board
+      .solve(n, board, 0)
+      .fold("Doesn't work lol\n")(Board.toString)
+  )
+}
